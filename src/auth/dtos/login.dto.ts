4100496b1a749c4d13ee31b4d666c96d0df1fde8
elizabetelissandra <elizabetelissandrasilva@gmail.com>
2024-12-05 19:37:58 -0300

    fix: :bug: Correção nos validators

--- src/auth/dtos/login.dto.ts
+++ src/auth/dtos/login.dto.ts
@@ -1,11 +1,11 @@
 import { IsEmail, IsNotEmpty, IsString } from "class-validator"
 
 export class LoginDto{
-    @IsEmail()
-    @IsNotEmpty()
+    @IsEmail({}, {message: 'Email is not valid'})
+    @IsNotEmpty({message: 'Email is required'})
     email: string
 
     @IsString()
-    @IsNotEmpty()
+    @IsNotEmpty({message: 'Password is required'})
     password: string
 }
